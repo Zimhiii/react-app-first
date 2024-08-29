0ac812ecfad41074f0cf4a9cc8f7e90e209990d8
Zimhiii <hijazmih@gmail.com>
2024-08-29 18:33:18 +0700

    first commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,8 @@
+// import React, { useState } from "react";
+import "../styles/Heading.css";
+
+function Heading(props) {
+  return <h1 className="props">{props.text}</h1>;
+}
+
+export default Heading;
